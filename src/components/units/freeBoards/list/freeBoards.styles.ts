0ca61b7590072cabb3383bb99f2711b3550cf51e
JJHaEun <yj03057@gmail.com>
2022-12-27 18:55:00 +0900

    freeboard병합

---
+++ src/components/units/freeBoards/list/freeBoards.styles.ts
@@ -0,0 +1,67 @@
+import styled from "@emotion/styled";
+
+export const OutWrapper = styled.div`
+padding: 200px;
+`
+export const Wrapper = styled.div`
+display: flex;
+margin-top: 100px;
+`
+export const TitleWrapper = styled.div`
+margin-left: 40px;
+`
+export const Title = styled.div`
+font-size: 60px;
+`
+export const FreeBoardWrapper = styled.div`
+margin: 40px;
+`
+export const BoardTitle = styled.div`
+font-style: normal;
+font-weight: 700;
+font-size: 40px;
+line-height: 100%;
+`
+export const Line = styled.div`
+width: 790px;
+height: 0px;
+border: 2px solid #e642aa;
+background-color: #e642aa;
+margin-top: 20px;
+`
+export const ContentsWrapper = styled.div`
+margin-top: 20px;
+display: flex;
+`
+export const ContentsViewWrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-end;
+`
+export const Contents = styled.div`
+font-size: 24px;
+margin-top: 50px;
+`
+export const View = styled.img`
+margin-top: 50px;
+width: 30px;
+height: 30px;
+`
+export const Number = styled.div`
+margin-top: 55px;
+width: 30px;
+height: 30px;
+`
+export const Image = styled.img`
+margin-top: 10px;
+`
+
+export const ContentsSubWrapper = styled.div`
+display: flex;
+flex-direction: column;
+width: 750px;
+`
+
+export const ViewWrapper = styled.div`
+display: flex;
+`
