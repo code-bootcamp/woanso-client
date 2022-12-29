6ccd51048a23306936755a2b7c7e3612f56b9df7
yujin08 <kkkyu28@naver.com>
2022-12-29 16:05:56 +0900

    [2022-12-29-(#35. 결제페이지 UI 생성)]

---
+++ src/components/units/payment/payment.styles.ts
@@ -0,0 +1,112 @@
+import styled from "@emotion/styled";
+
+export const OutWrapper = styled.div`
+`
+export const InnerWrapper = styled.div`
+margin: 100px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-around;
+`
+export const TitleWrapper = styled.div`
+margin-bottom: 50px;
+`
+export const Title = styled.div`
+font-size: 32px;
+text-align: center;
+`
+export const ContentsWrapper = styled.div`
+display: flex;
+flex-direction: column;
+margin-bottom: 100px;
+`
+export const ContentsTitle = styled.div`
+margin-bottom: 50px;
+`
+export const Label = styled.div`
+margin-bottom: 30px;
+`
+export const SubWrapper = styled.div`
+display: flex;
+margin-top: 10px;
+`
+export const Input = styled.div`
+background-color: #D9D9D9;
+margin-right: 40px;
+width: 200px;
+height: 40px;
+margin-bottom: 40px;
+padding: 10px;
+`
+export const Input2 = styled.div`
+background-color: #D9D9D9;
+width: 440px;
+height: 40px;
+margin-bottom: 20px;
+padding: 10px;
+`
+
+export const Input3 = styled.div`
+background-color: #D9D9D9;
+width: 300px;
+height: 40px;
+margin-bottom: 20px;
+padding: 10px;
+margin-right: 60px;
+`
+
+export const Input4 = styled.div`
+background-color: #D9D9D9;
+width: 80px;
+height: 40px;
+margin-bottom: 20px;
+padding: 10px;
+margin-right: 30px;
+`
+
+
+export const Input5 = styled.div`
+background-color: #D9D9D9;
+width: 130px;
+height: 80px;
+`
+
+export const SubWrapper3 = styled.div`
+display: flex;
+flex-direction: column;
+margin-left: 10px;
+`
+
+export const SubWrapper4 = styled.div`
+margin-left: 160px;
+margin-top: 40px;
+`
+
+export const Line = styled.div`
+width: 460px;
+height: 0px;
+border: 1px solid black;
+`
+
+export const ContentsWrapper2 = styled.div`
+display: flex;
+flex-direction: column;
+margin-bottom: 40px;
+align-items: flex-end;
+margin-left: 300px;
+`
+
+export const Label2 = styled.div`
+margin-bottom: 30px;
+margin-left: 30px;
+`
+
+export const Button = styled.div`
+background-color: #77170D;
+color: white;
+width: 400px;
+height: 40px;
+text-align: center;
+padding-top: 10px;
+`
