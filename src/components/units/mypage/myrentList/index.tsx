65b14f61613460ba94c8fe3ab90de727cff9de09
JJHaEun <yj03057@gmail.com>
2022-12-30 20:22:43 +0900

    2022-12-30-(#58.Mypage 대여목록 UI구현)

--- src/components/units/mypage/myrentList/index.tsx
+++ src/components/units/mypage/myrentList/index.tsx
@@ -1,25 +1,62 @@
 import * as S from "./style";
 
+const RentList = [
+  {
+    rentDate: "2022/12/29",
+    bookTitle: "제목",
+    price: "12000",
+    paymentWay: "포인트결제",
+    paymentStatus: "결제완료",
+  },
+  {
+    rentDate: "2022/12/29",
+    bookTitle: "제목1",
+    price: "10000",
+    paymentWay: "카드결제",
+    paymentStatus: "결제완료",
+  },
+  {
+    rentDate: "2022/12/29",
+    bookTitle: "제목2",
+    price: "가격",
+    paymentWay: "카드결제",
+    paymentStatus: "결제완료",
+  },
+  {
+    rentDate: "2022/12/29",
+    bookTitle: "제목3fasdfsadfasgdga",
+    price: "가격",
+    paymentWay: "포인트결제",
+    paymentStatus: "결제완료",
+  },
+];
 export default function MyRentListUI() {
   return (
-    <div>
-      <h1>대여내역</h1>
-      <S.RentDetailRow>
-        <S.RentBook>
-          <S.RentBookImg src="/action/01.jpeg" />
-        </S.RentBook>
-        <div>2022/12/29</div>
-        <div>
-          <div>
-            <div>책제목</div>
-            <div>10000 원</div>
-          </div>
-          <div>
-            <div>결제방법</div>
-            <div>결제상태</div>
-          </div>
-        </div>
-      </S.RentDetailRow>
-    </div>
+    <S.RentList>
+      <S.RentMain>
+        <S.RentTitle>대여내역</S.RentTitle>
+
+        {RentList.map((el) => (
+          <S.RentDetailRow>
+            <S.RentBook>
+              <S.RentBookImg src="/action/01.jpeg" />
+            </S.RentBook>
+            <S.Detail>
+              <S.Date>{el.rentDate}</S.Date>
+              <S.Rents>
+                <S.TitlePrice>
+                  <S.BookTitle>{el.bookTitle}</S.BookTitle>
+                  <div>{el.price} 원</div>
+                </S.TitlePrice>
+                <S.PaymentStatus>
+                  <S.Way>{el.paymentWay}</S.Way>
+                  <div>{el.paymentStatus}</div>
+                </S.PaymentStatus>
+              </S.Rents>
+            </S.Detail>
+          </S.RentDetailRow>
+        ))}
+      </S.RentMain>
+    </S.RentList>
   );
 }
--- src/components/units/mypage/myrentList/style.ts
+++ src/components/units/mypage/myrentList/style.ts
@@ -1,14 +1,68 @@
 import styled from "@emotion/styled";
 
+export const RentList = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 120px;
+`;
+export const RentMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const RentTitle = styled.h1`
+  margin-bottom: 20px;
+`;
 export const RentDetailRow = styled.div`
   display: flex;
+  width: 1350px;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  padding: 30px 15px;
+
+  border-top: 1px solid rgba(0, 0, 0, 0.3);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
 `;
 export const RentBook = styled.div`
   width: 226px;
   height: 162px;
+  background-color: #ffffff;
 `;
 export const RentBookImg = styled.img`
   width: 100%;
   height: 100%;
   object-fit: contain;
+`;
+export const Detail = styled.div`
+  display: flex;
+  /* gap: 250px; */
+  margin-left: 100px;
+`;
+export const Rents = styled.div`
+  display: flex;
+`;
+export const TitlePrice = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+export const PaymentStatus = styled.div`
+  display: flex;
+`;
+export const Date = styled.div`
+  margin-right: 120px;
+`;
+export const BookTitle = styled.div`
+  width: 200px;
+
+  margin-right: 200px;
+`;
+export const Way = styled.div`
+  margin-right: 100px;
+
+  width: 150px;
+`;
+export const Status = styled.div`
+  width: 150px;
 `;
