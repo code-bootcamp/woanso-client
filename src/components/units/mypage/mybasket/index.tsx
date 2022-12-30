fa898543d77d86cb7a0d37f527fe535f490d4cc7
JJHaEun <yj03057@gmail.com>
2022-12-30 09:55:13 +0900

    임시저장

--- src/components/units/mypage/mybasket/index.tsx
+++ src/components/units/mypage/mybasket/index.tsx
@@ -1,47 +1,61 @@
+import * as S from "./style";
+
 export default function MyBasketUI() {
   return (
-    <div>
-      <h1>장바구니</h1>
-      <div>
-        <input type="checkbox" />
-        <div>구매 / 삭제</div>
-      </div>
-      <div>
-        <input type="checkbox" />
-        <img />
+    <S.MyBasket>
+      <S.BasketList>
+        <h1>장바구니</h1>
+        <S.TopRow>
+          <input type="checkbox" />
+          <div>구매 / 삭제</div>
+        </S.TopRow>
+        <S.ListRow>
+          <input type="checkbox" />
+          <S.ListContents>
+            <div>
+              <img src="/action/02.jpeg" />
+            </div>
+            <S.List>
+              <div>출판사명</div>
+              <div>상품이름</div>
+              <div>가격</div>
+            </S.List>
+            <div>
+              <button>구매</button>
+              <button>삭제</button>
+            </div>
+          </S.ListContents>
+        </S.ListRow>
         <div>
-          <div>출판사명</div>
-          <div>상품이름</div>
-          <div>가격</div>
-        </div>
-        <button>구매</button>
-        <button>삭제</button>
-      </div>
-      <div>
-        <input type="checkbox" />
-        <img />
-        <div>
-          <div>출판사명</div>
-          <div>상품이름</div>
-          <div>대여 불가능</div>
-        </div>
-        <button>구매</button>
-        <button>삭제</button>
-      </div>
-      <div>
-        <div>
-          <div>상품가격</div>
-          <div>10000원</div>
+          <input type="checkbox" />
+          <div>
+            <div>
+              <img src="/action/02.jpeg" />
+            </div>
+            <div>
+              <div>출판사명</div>
+              <div>상품이름</div>
+              <div>가격</div>
+            </div>
+          </div>
+          <button>구매</button>
+          <button>삭제</button>
         </div>
         <div>
-          <div>배송비</div>
-          <div>3000원</div>
+          <div>
+            <div>상품가격</div>
+            <div>10000원</div>
+          </div>
+          <div>
+            <div>배송비</div>
+            <div>3000원</div>
+          </div>
+          <div>
+            <div>총 결제금액</div>
+            <div>13000원</div>
+          </div>
         </div>
-        <div>
-          <div>총 결제금액</div>
-          <div>13000원</div>
-        </div>
-      </div>
-    </div>
+      </S.BasketList>
+    </S.MyBasket>
   );
 }
---
+++ src/components/units/mypage/mybasket/style.ts
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+
+export const MyBasket = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: red;
+`;
+export const BasketList = styled.div`
+  border: 1px solid black;
+`;
+export const TopRow = styled.div`
+  display: flex;
+  gap: 1000px;
+`;
+export const ListRow = styled.div`
+  display: flex;
+`;
+export const List = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const ListContents = styled.div`
+  display: flex;
+`;
