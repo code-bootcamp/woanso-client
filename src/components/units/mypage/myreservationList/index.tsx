b06b5a397ab2c5c26e25dccbc8b7eb592cd7bfc4
JJHaEun <yj03057@gmail.com>
2023-01-01 21:30:26 +0900

    2023-01-01-(#70.대여목록 카드형으로 변경)

--- src/components/units/mypage/myreservationList/index.tsx
+++ src/components/units/mypage/myreservationList/index.tsx
@@ -1,3 +1,5 @@
+import { faCircleXmark } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as S from "./style";
 
 const ResevationLists = [
@@ -19,32 +21,42 @@ const ResevationLists = [
     price: "35000",
     due: "2022/12/25~12/31",
   },
+  {
+    number: "23456546",
+    title: "명탐정 코난",
+    price: "35000",
+    due: "2022/12/25~12/31",
+  },
 ];
 export default function MyReservationListUI() {
   return (
     <S.ReservationBox>
       <S.ReservationList>
         <h1>예약내역</h1>
-        <div>
-          <S.TopRowReservation>
-            <S.Number>예약번호</S.Number>
-            <span>상품명</span>
-            <S.Price>상품가격</S.Price>
-            <span>예약기간</span>
-          </S.TopRowReservation>
+        <S.Wrap>
           {ResevationLists.map((el) => (
-            <S.ReservationDetail key={el.number}>
-              <S.NumberAndTitle>
-                <S.Icon />
-                <S.ReservationNumber>{el.number}</S.ReservationNumber>
-                <S.ReservationBookTitle>{el.title}</S.ReservationBookTitle>
-              </S.NumberAndTitle>
-              <S.ReservationPrice> {el.price}</S.ReservationPrice>
-              <S.ReservationPeriod>{el.due}</S.ReservationPeriod>
-              <S.Cancel>취소</S.Cancel>
-            </S.ReservationDetail>
+            <S.ReservationWrapper>
+              <S.TopRowReservation>
+                <S.Number>예약번호</S.Number>
+                <span>상품명</span>
+                <S.Price>상품가격</S.Price>
+                <span>예약기간</span>
+              </S.TopRowReservation>
+
+              <S.ReservationDetail key={el.number}>
+                <S.NumberAndTitle>
+                  <S.ReservationNumber>{el.number}</S.ReservationNumber>
+                  <S.ReservationBookTitle>{el.title}</S.ReservationBookTitle>
+                </S.NumberAndTitle>
+                <S.ReservationPrice> {el.price}</S.ReservationPrice>
+                <S.ReservationPeriod>{el.due}</S.ReservationPeriod>
+                <S.Cancel>
+                  <FontAwesomeIcon icon={faCircleXmark} />
+                </S.Cancel>
+              </S.ReservationDetail>
+            </S.ReservationWrapper>
           ))}
-        </div>
+        </S.Wrap>
       </S.ReservationList>
     </S.ReservationBox>
   );
--- src/components/units/mypage/myreservationList/style.ts
+++ src/components/units/mypage/myreservationList/style.ts
@@ -1,77 +1,86 @@
-import { EllipsisOutlined } from "@ant-design/icons";
+// import { EllipsisOutlined } from "@ant-design/icons";
 import styled from "@emotion/styled";
 
 export const ReservationBox = styled.div`
   display: flex;
   justify-content: center;
-
   margin-top: 100px;
 `;
 export const ReservationList = styled.div`
-  width: auto;
+  width: 1800px;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
-
+export const ReservationWrapper = styled.div`
+  width: 850px;
+  height: 200px;
+  border-radius: 10px;
+  box-shadow: 5px 5px 15px 8px rgba(0, 0, 0, 0.3);
+  padding: 0 25px 0 20px;
+  margin: 0 40px 40px 0;
+`;
+export const Wrap = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
 export const TopRowReservation = styled.div`
   display: flex;
   justify-content: center;
 
-  border-top: 1px solid rgba(0, 0, 0, 0.3);
   border-bottom: 1px solid rgba(0, 0, 0, 0.3);
-  gap: 260px;
+  /* gap: 200px; */
   font-size: 25px;
   padding: 15px 0;
 `;
 export const Number = styled.span`
-  width: 120px;
-  margin-left: 50px;
+  width: 180px;
+  /* margin-left: 100px; */
+  /* margin-right: 100px; */
 `;
 export const Price = styled.span`
-  width: 180px;
-  padding-left: 80px;
+  width: 200px;
+  margin-left: 100px;
 `;
-export const Icon = styled(EllipsisOutlined)`
-  width: 40px;
-  padding-left: 120px;
-  * {
-    font-size: 25px;
-  }
-`;
+// export const Icon = styled(EllipsisOutlined)`
+//   width: 40px;
+//   padding-left: 120px;
+//   * {
+//     font-size: 25px;
+//   }
+// `;
 export const ReservationDetail = styled.div`
   display: flex;
   justify-content: center;
-  gap: 80px;
-  padding: 20px 0px;
+  gap: 60px;
+  padding: 20px 0;
   text-align: center;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
 `;
 
 export const ReservationNumber = styled.span`
   width: 150px;
   font-size: 20px;
-  margin-left: 95px;
+  margin-left: 50px;
 `;
 export const ReservationBookTitle = styled.span`
-  width: 150px;
+  width: 100px;
 
-  margin-left: 120px;
+  margin-left: 30px;
   font-size: 20px;
 `;
 export const NumberAndTitle = styled.div`
   display: flex;
-  gap: 120px;
 `;
 export const ReservationPrice = styled.span`
   width: 150px;
-  margin-left: 180px;
+  margin-left: 50px;
   font-size: 20px;
 `;
 export const ReservationPeriod = styled.span`
   width: 250px;
 
-  margin-left: 100px;
+  /* margin-left: 10px; */
 `;
 export const Cancel = styled.button`
   width: 130px;
