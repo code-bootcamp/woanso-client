33b946520434382a9ddf835fc23239d00a773e5d
JJHaEun <114386587+JJHaEun@users.noreply.github.com>
2023-01-02 10:46:45 +0900

    feature #33 포인트 내역 UI구현  (#77)
    * 2023-01-01-(#33.포인트 내역 UI구현)
    
    * 2023-01-01-(#33.포인트 내역 UI구현 버튼 넣었다 삭제함)
    
    Co-authored-by: JJHaEun <yj03057@gmail.com>

---
+++ src/components/units/mypage/mypoint/index.tsx
@@ -0,0 +1,51 @@
+import * as S from "./style";
+
+const Point = [
+  {
+    creatAt: "2022/12/31",
+    status: "사용",
+    amount: "35000",
+    usedItem: "상품 구매",
+    balance: "3000",
+  },
+  {
+    creatAt: "2022/11/29",
+    status: "충전",
+    amount: "3000",
+    usedItem: "이벤트 지급",
+    balance: "38000",
+  },
+  {
+    creatAt: "2022/10/13",
+    status: "충전",
+    amount: "35000",
+    usedItem: "포인트 충전",
+    balance: "35000",
+  },
+];
+
+export default function MyPointUI() {
+  return (
+    <S.PointWrapper>
+      <S.PointMainList>
+        <h1>포인트내역</h1>
+        <S.TopRow>
+          <S.Status>생성일</S.Status>
+          <S.Status>상태(충전/사용)</S.Status>
+          <S.Amount>금액(p)</S.Amount>
+          <S.Status>사용내역</S.Status>
+          <S.Balance>잔액(p)</S.Balance>
+        </S.TopRow>
+        {Point.map((el) => (
+          <S.PointList>
+            <S.Status>{el.creatAt}</S.Status>
+            <S.Status>{el.status}</S.Status>
+            <S.Amount>{el.amount}</S.Amount>
+            <S.Status>{el.usedItem}</S.Status>
+            <S.Balance>{el.balance}</S.Balance>
+          </S.PointList>
+        ))}
+      </S.PointMainList>
+    </S.PointWrapper>
+  );
+}
