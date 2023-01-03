59bb5fd52f753b324333b31bc8c520f6bc087abf
JJHaEun <yj03057@gmail.com>
2023-01-03 10:08:11 +0900

    2023-01-03-(#100.결제버튼 클릭시 훼손 및 분실 & 취소 환불 관련 모달뜨게함.)

---
+++ src/components/commons/customModal/rentcheckModal/index.tsx
@@ -0,0 +1,64 @@
+import { Button, Modal } from "antd";
+import { useRouter } from "next/router";
+import { Dispatch, SetStateAction } from "react";
+import * as S from "./style";
+interface ModalProps {
+  isModalOpen: boolean;
+  setIsModalOpen: Dispatch<SetStateAction<boolean>>;
+}
+export default function RentCheckModal(props: ModalProps) {
+  const router = useRouter();
+
+  const handleOk = () => {
+    props.setIsModalOpen(false);
+    console.log("결제하기 API날리기");
+  };
+  return (
+    <>
+      <S.ModalWrap
+        centered
+        // title="책 훼손, 또는 분실시"
+        open={props.isModalOpen}
+        onOk={handleOk}
+        footer={[<Button onClick={handleOk}>확인</Button>]}
+      >
+        <S.InFoWrap>
+          <S.Titles>
+            <h1>훼손 및 분실</h1>
+          </S.Titles>
+          <S.UlList>
+            <div>
+              <li>대여한 도서의 페이지 훼손이나 분실 발생시에는</li>
+              <li>추가비용이 청구될 수 있습니다.</li>
+              <li>(단, 택배사가 분실 시 택배사가 배상합니다) </li>
+            </div>
+            <li>
+              지정된 반납일에 반납이 안 될 경우에는 일당 연체료가 청구됩니다
+            </li>
+          </S.UlList>
+          <S.Titles>
+            <h1>취소 및 환불 안내</h1>
+          </S.Titles>
+          <S.UlList>
+            <li>- 배송 출고 후에는 취소 및 환불이 불가합니다.</li>
+            <div>
+              <li>- 사정이 생겨 취소 혹은 날짜 변경을 원하실 경우 </li>
+              <li>배송 출고 전 빠른 연락 부탁드립니다.</li>
+            </div>
+            <li>
+              - 출고 작업은 오전 10시에 마감됩니다. 이 점 참고하여 주시기
+              바랍니다.
+            </li>
+          </S.UlList>
+          <S.AskTable>
+            <h2>자세한 사항은</h2>
+            <a href="https://open.kakao.com/o/s8iiqXVe">
+              <S.AskKaKao>1:1문의하기</S.AskKaKao>
+            </a>
+            <h2>를 이용해 주시기 바랍니다</h2>
+          </S.AskTable>
+        </S.InFoWrap>
+      </S.ModalWrap>
+    </>
+  );
+}
--- src/components/units/payment/payment.container.tsx
+++ src/components/units/payment/payment.container.tsx
@@ -1,82 +1,91 @@
-import * as S from "./payment.styles"
+import { useState } from "react";
+import RentCheckModal from "../../commons/customModal/rentcheckModal";
+import * as S from "./payment.styles";
 
-export default function Payment(){
-    return (
-        <S.OutWrapper>
-            <S.InnerWrapper>
-                <S.TitleWrapper>
-                    <S.Title>결제하기</S.Title>
-                </S.TitleWrapper>
-                <S.ContentsWrapper>
-                    <S.ContentsTitle>주문/배송 정보</S.ContentsTitle>
-                    <S.Label>주문자</S.Label>
-                    <S.SubWrapper>
-                        <S.Input placeholder="이름"></S.Input>
-                        <S.Input placeholder="전화번호"></S.Input>
-                    </S.SubWrapper>
-                        <S.Input2 placeholder="이메일"></S.Input2>
-                </S.ContentsWrapper>
-                <S.ContentsWrapper>
-                    <S.Label>배송지 정보</S.Label>
-                    <S.SubWrapper>
-                        <S.Input placeholder="이름"></S.Input>
-                        <S.Input placeholder="전화번호"></S.Input>
-                    </S.SubWrapper>
-                    <S.Input2 placeholder="주소"></S.Input2>
-                    <S.Input2 placeholder="상세주소"></S.Input2>
-                </S.ContentsWrapper>
-                <S.ContentsWrapper>
-                    <S.Label>쿠폰</S.Label>
-                    <S.SubWrapper>
-                        <S.CouponPrice>원</S.CouponPrice>
-                            <S.Coupon>쿠폰적용</S.Coupon>
-                    </S.SubWrapper>
-                </S.ContentsWrapper>
-                <S.ContentsWrapper>
-                    <S.Label>주문 상품 정보</S.Label>
-                    <S.SubWrapper>
-                    <S.SubWrapper>
-                        <S.Input5></S.Input5>
-                    </S.SubWrapper>
-                    <S.SubWrapper3>
-                        <S.Label>만화책 제목</S.Label>
-                        <S.Label>총 1개</S.Label>
-                    </S.SubWrapper3>
-                    <S.SubWrapper4>
-                    <S.Label>18,000원</S.Label>
-                    </S.SubWrapper4>
-                    </S.SubWrapper>
-                    <S.Line></S.Line>
-                </S.ContentsWrapper>
-                <S.ContentsWrapper2>
-                    <S.SubWrapper>
-                    <S.Label>상품가격</S.Label>
-                    <S.Label2>18,000원</S.Label2>
-                    </S.SubWrapper>
-                    <S.SubWrapper>
-                    <S.Label>배송비</S.Label>
-                    <S.Label2>+2,500원</S.Label2>
-                    </S.SubWrapper>
-                    <S.SubWrapper>
-                    <S.Label>쿠폰 사용</S.Label>
-                    <S.Label2>-1,000원</S.Label2>
-                    </S.SubWrapper>
-                    <S.SubWrapper>
-                    <S.Label>보증금</S.Label>
-                    <S.Label2>50,000원</S.Label2>
-                    </S.SubWrapper>
-                </S.ContentsWrapper2>
-                <S.Line></S.Line>
-                <S.ContentsWrapper2>
-                <S.SubWrapper>
-                <S.Label>총 결제금액</S.Label>
-                    <S.Label2>69,500원</S.Label2>
-                    </S.SubWrapper>
-                </S.ContentsWrapper2>
-                <S.Button>
-                    결제하기
-                </S.Button>
-            </S.InnerWrapper>
-        </S.OutWrapper>
-    )
+export default function Payment() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  return (
+    <S.OutWrapper>
+      <RentCheckModal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      />
+
+      <S.InnerWrapper>
+        <S.TitleWrapper>
+          <S.Title>결제하기</S.Title>
+        </S.TitleWrapper>
+        <S.ContentsWrapper>
+          <S.ContentsTitle>주문/배송 정보</S.ContentsTitle>
+          <S.Label>주문자</S.Label>
+          <S.SubWrapper>
+            <S.Input placeholder="이름"></S.Input>
+            <S.Input placeholder="전화번호"></S.Input>
+          </S.SubWrapper>
+          <S.Input2 placeholder="이메일"></S.Input2>
+        </S.ContentsWrapper>
+        <S.ContentsWrapper>
+          <S.Label>배송지 정보</S.Label>
+          <S.SubWrapper>
+            <S.Input placeholder="이름"></S.Input>
+            <S.Input placeholder="전화번호"></S.Input>
+          </S.SubWrapper>
+          <S.Input2 placeholder="주소"></S.Input2>
+          <S.Input2 placeholder="상세주소"></S.Input2>
+        </S.ContentsWrapper>
+        <S.ContentsWrapper>
+          <S.Label>쿠폰</S.Label>
+          <S.SubWrapper>
+            <S.CouponPrice>원</S.CouponPrice>
+            <S.Coupon>쿠폰적용</S.Coupon>
+          </S.SubWrapper>
+        </S.ContentsWrapper>
+        <S.ContentsWrapper>
+          <S.Label>주문 상품 정보</S.Label>
+          <S.SubWrapper>
+            <S.SubWrapper>
+              <S.Input5></S.Input5>
+            </S.SubWrapper>
+            <S.SubWrapper3>
+              <S.Label>만화책 제목</S.Label>
+              <S.Label>총 1개</S.Label>
+            </S.SubWrapper3>
+            <S.SubWrapper4>
+              <S.Label>18,000원</S.Label>
+            </S.SubWrapper4>
+          </S.SubWrapper>
+          <S.Line></S.Line>
+        </S.ContentsWrapper>
+        <S.ContentsWrapper2>
+          <S.SubWrapper>
+            <S.Label>상품가격</S.Label>
+            <S.Label2>18,000원</S.Label2>
+          </S.SubWrapper>
+          <S.SubWrapper>
+            <S.Label>배송비</S.Label>
+            <S.Label2>+2,500원</S.Label2>
+          </S.SubWrapper>
+          <S.SubWrapper>
+            <S.Label>쿠폰 사용</S.Label>
+            <S.Label2>-1,000원</S.Label2>
+          </S.SubWrapper>
+          <S.SubWrapper>
+            <S.Label>보증금</S.Label>
+            <S.Label2>50,000원</S.Label2>
+          </S.SubWrapper>
+        </S.ContentsWrapper2>
+        <S.Line></S.Line>
+        <S.ContentsWrapper2>
+          <S.SubWrapper>
+            <S.Label>총 결제금액</S.Label>
+            <S.Label2>69,500원</S.Label2>
+          </S.SubWrapper>
+        </S.ContentsWrapper2>
+        <S.Button onClick={showModal}>결제하기</S.Button>
+      </S.InnerWrapper>
+    </S.OutWrapper>
+  );
 }
