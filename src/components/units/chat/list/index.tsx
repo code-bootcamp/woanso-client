4912ce7739598dfaeb6702387c0986196f33d357
2zzzyoung <mzooyoung@gmail.com>
2023-01-03 16:37:01 +0900

    feature #104 채팅방UI 수정

--- src/components/units/chat/list/index.tsx
+++ src/components/units/chat/list/index.tsx
@@ -5,11 +5,9 @@ export default function ChatList() {
     <>
       <S.OuterWrap>
         <S.InnerWrap>
+          <S.DateBox>2023/1/3</S.DateBox>
           <S.ChatWrap>
-            <img
-              src="/icon/chat.png"
-              style={{ width: "60px", height: "60px" }}
-            />
+            <img src="/icon/son.png" />
             <S.ChatBox>
               <S.ChatContents>안녕하세요</S.ChatContents>
             </S.ChatBox>
@@ -18,26 +16,23 @@ export default function ChatList() {
             <S.MyChat>네 혹시 짱구 보셨나요?</S.MyChat>
           </S.MyChatBox>
           <S.ChatWrap>
-            <img
-              src="/icon/chat.png"
-              style={{ width: "60px", height: "60px" }}
-            />
+            <img src="/icon/son.png" />
             <S.ChatBox>
               <S.ChatContents>당연히 봤져</S.ChatContents>
             </S.ChatBox>
           </S.ChatWrap>
           <S.ChatWrap>
-            <img
-              src="/icon/chat.png"
-              style={{ width: "60px", height: "60px" }}
-            />
+            <img src="/icon/son.png" />
             <S.ChatBox>
               <S.ChatContents>다른 거 추천 좀 해주세용</S.ChatContents>
             </S.ChatBox>
+            <S.ChatTime>2:49 pm</S.ChatTime>
           </S.ChatWrap>
           <S.TextareaBox>
-            <S.WriteBox placeholder="message" />
-            <S.ChatBtn>전송</S.ChatBtn>
+            <S.WriteBox placeholder="Enter a chat" />
+            <S.ChatBtn>
+              <img src="/icon/send2.png" />
+            </S.ChatBtn>
           </S.TextareaBox>
         </S.InnerWrap>
       </S.OuterWrap>
