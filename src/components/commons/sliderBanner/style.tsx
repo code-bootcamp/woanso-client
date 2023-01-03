66a3fdfa9ef633cdad57e94207c014548d948faa
MyeongheeJung <dobby.jmh@gmail.com>
2023-01-03 18:36:39 +0900

    [2022-01-03(sliderBannerUI 추가)]

---
+++ src/components/commons/sliderBanner/style.tsx
@@ -0,0 +1,39 @@
+import styled from "@emotion/styled";
+import Slider from "react-slick";
+
+export const OuterWrap = styled.div``;
+
+export const InnerWrap = styled.div``;
+
+export const SliderImgWrap = styled.div`
+  width: 100%;
+`;
+
+export const SliderImg = styled.img`
+  width: 100%;
+`;
+
+export const SliderWrap = styled(Slider)`
+  .slick-initialized {
+    position: relative;
+  }
+
+  .slick-prev:before,
+  .slick-next:before {
+    color: white;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  .slick-prev {
+    z-index: 10;
+  }
+  .slick-prev:before {
+    left: 50px;
+  }
+
+  .slick-next:before {
+    right: 50px;
+  }
+`;
