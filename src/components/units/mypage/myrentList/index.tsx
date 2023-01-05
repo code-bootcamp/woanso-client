4af897d9295433295db68cbb2662722fd605ec10
yujin08 <kkkyu28@naver.com>
2023-01-05 18:33:43 +0900

    [2023-01-05-(#167. 대여내역 페이지 UI 수정)]

--- src/components/units/mypage/myrentList/index.tsx
+++ src/components/units/mypage/myrentList/index.tsx
@@ -1,3 +1,4 @@
+import { SubmitButton3, SubmitButton2 } from "../../../../commons/styles/Button";
 import { OuterWrap, InnerWrap } from "../../../../commons/styles/Wrapper";
 import * as S from "./style";
 
@@ -6,26 +7,130 @@ export default function MyRentListUI() {
   return (
     <OuterWrap>
       <InnerWrap>
-        <S.Wrap>
           <S.TitleWrap>
             <S.Title>대여내역</S.Title>
           </S.TitleWrap>
+        <S.Wrap>
+        {/* <S.dfskf2>
+            <S.OFDjsfdos>sdkf</S.OFDjsfdos>
+          </S.dfskf2> */}
+          <S.dfsd>
           <S.ContentsWrap>
+          <S.Number>No. 2018061211243416525</S.Number>
+            <S.RentWrap>
             <S.ImgWrap>
               <S.Img src="/item1.png"></S.Img>
             </S.ImgWrap>
-            <S.RentWrap>
-              <S.RentDate>2023.01.05 대여</S.RentDate>
+            <S.Wrap1>
+            <S.RentDate>2023.01.05 대여</S.RentDate>
               <S.RentTitle>순정만화임당</S.RentTitle>
               <S.RentWriter>자까</S.RentWriter>
               <S.RentPrice>15,000원</S.RentPrice>
+            </S.Wrap1>
             </S.RentWrap>
             <S.ButtonWrap>
-              <S.WishIcon></S.WishIcon>
-              <S.ReviewButton>리뷰쓰기</S.ReviewButton>
-              <S.RentButton>재대여</S.RentButton>
+              {/* <SubmitButton2>위시리스트</SubmitButton2> */}
+              <SubmitButton2>리뷰쓰기</SubmitButton2>
+              <SubmitButton2>재대여</SubmitButton2>
             </S.ButtonWrap>
           </S.ContentsWrap>
+          <S.ContentsWrap>
+          <S.Number>No. 2018061211243416525</S.Number>
+            <S.RentWrap>
+            <S.ImgWrap>
+              <S.Img src="/item2.png"></S.Img>
+            </S.ImgWrap>
+            <S.Wrap1>
+            <S.RentDate>2023.01.05 대여</S.RentDate>
+              <S.RentTitle>순정만화임당</S.RentTitle>
+              <S.RentWriter>자까</S.RentWriter>
+              <S.RentPrice>15,000원</S.RentPrice>
+            </S.Wrap1>
+            </S.RentWrap>
+            <S.ButtonWrap>
+              {/* <SubmitButton2>위시리스트</SubmitButton2> */}
+              <SubmitButton2>리뷰쓰기</SubmitButton2>
+              <SubmitButton2>재대여</SubmitButton2>
+            </S.ButtonWrap>
+          </S.ContentsWrap>
+          <S.ContentsWrap>
+          <S.Number>No. 2018061211243416525</S.Number>
+            <S.RentWrap>
+            <S.ImgWrap>
+              <S.Img src="/item3.png"></S.Img>
+            </S.ImgWrap>
+            <S.Wrap1>
+            <S.RentDate>2023.01.05 대여</S.RentDate>
+              <S.RentTitle>순정만화임당</S.RentTitle>
+              <S.RentWriter>자까</S.RentWriter>
+              <S.RentPrice>15,000원</S.RentPrice>
+            </S.Wrap1>
+            </S.RentWrap>
+            <S.ButtonWrap>
+              {/* <SubmitButton2>위시리스트</SubmitButton2> */}
+              <SubmitButton2>리뷰쓰기</SubmitButton2>
+              <SubmitButton2>재대여</SubmitButton2>
+            </S.ButtonWrap>
+          </S.ContentsWrap>
+          <S.ContentsWrap>
+          <S.Number>No. 2018061211243416525</S.Number>
+            <S.RentWrap>
+            <S.ImgWrap>
+              <S.Img src="/item1.png"></S.Img>
+            </S.ImgWrap>
+            <S.Wrap1>
+            <S.RentDate>2023.01.05 대여</S.RentDate>
+              <S.RentTitle>순정만화임당</S.RentTitle>
+              <S.RentWriter>자까</S.RentWriter>
+              <S.RentPrice>15,000원</S.RentPrice>
+            </S.Wrap1>
+            </S.RentWrap>
+            <S.ButtonWrap>
+              {/* <SubmitButton2>위시리스트</SubmitButton2> */}
+              <SubmitButton2>리뷰쓰기</SubmitButton2>
+              <SubmitButton2>재대여</SubmitButton2>
+            </S.ButtonWrap>
+          </S.ContentsWrap>
+          <S.ContentsWrap>
+          <S.Number>No. 2018061211243416525</S.Number>
+            <S.RentWrap>
+            <S.ImgWrap>
+              <S.Img src="/item2.png"></S.Img>
+            </S.ImgWrap>
+            <S.Wrap1>
+            <S.RentDate>2023.01.05 대여</S.RentDate>
+              <S.RentTitle>순정만화임당</S.RentTitle>
+              <S.RentWriter>자까</S.RentWriter>
+              <S.RentPrice>15,000원</S.RentPrice>
+            </S.Wrap1>
+            </S.RentWrap>
+            <S.ButtonWrap>
+              {/* <SubmitButton2>위시리스트</SubmitButton2> */}
+              <SubmitButton2>리뷰쓰기</SubmitButton2>
+              <SubmitButton2>재대여</SubmitButton2>
+            </S.ButtonWrap>
+          </S.ContentsWrap>
+          <S.ContentsWrap>
+          <S.Number>No. 2018061211243416525</S.Number>
+            <S.RentWrap>
+            <S.ImgWrap>
+              <S.Img src="/item3.png"></S.Img>
+            </S.ImgWrap>
+            <S.Wrap1>
+            <S.RentDate>2023.01.05 대여</S.RentDate>
+              <S.RentTitle>순정만화임당</S.RentTitle>
+              <S.RentWriter>자까</S.RentWriter>
+              <S.RentPrice>15,000원</S.RentPrice>
+            </S.Wrap1>
+            </S.RentWrap>
+            <S.ButtonWrap>
+              {/* <SubmitButton2>위시리스트</SubmitButton2> */}
+              <SubmitButton2>리뷰쓰기</SubmitButton2>
+              <SubmitButton2>재대여</SubmitButton2>
+            </S.ButtonWrap>
+          </S.ContentsWrap>
+          </S.dfsd>
+       
         </S.Wrap>
       </InnerWrap>
     </OuterWrap>
--- src/components/units/mypage/myrentList/style.ts
+++ src/components/units/mypage/myrentList/style.ts
@@ -1,31 +1,77 @@
 import styled from "@emotion/styled";
 
-export const Wrap = styled.div``
+export const Wrap = styled.div`
+display: flex;
+`
 export const TitleWrap = styled.div`
 text-align: center;
+margin-bottom: 50px;
 `
 export const Title = styled.div`
 font-size: 28px;
 `
 export const ContentsWrap = styled.div`
+width: 400px;
 display: flex;
-background: gray;
-/* justify-content: flex-end; */
+justify-content: space-between;
+flex-direction: column;
+border: 1px solid #ccc;
+border-radius: 10px;
+margin-bottom: 10px;
 `
 export const ImgWrap = styled.div``
 export const Img = styled.img`
-width: 200px;
-height: 250px;
+width: 150px;
+height: 200px;
+border: 1px solid #ccc;
 `
-export const RentWrap = styled.div``
-export const RentDate = styled.div``
-export const RentTitle = styled.div``
+export const RentWrap = styled.div`
+display: flex;
+gap: 20px;
+padding: 40px;
+`
+export const RentDate = styled.div`
+color: gray;
+`
+export const RentTitle = styled.div`
+font-size: 22px;
+`
 export const RentWriter = styled.div``
-export const RentPrice = styled.div``
-export const ButtonWrap = styled.div`
-background: blue;
-/* justify-content: flex-end; */
+export const RentPrice = styled.div`
+font-size: 20px;
 `
-export const WishIcon = styled.div``
+export const ButtonWrap = styled.div`
+display: flex;
+gap: 20px;
+padding: 0 40px 20px 40px;
+`
+export const WishIcon = styled.img`
+width: 30px;
+`
 export const ReviewButton = styled.div``
 export const RentButton = styled.div``
+
+export const Wrap1 = styled.div`
+/* background-color: gray; */
+display: flex;
+flex-direction: column;
+justify-content: center;
+gap: 5px;
+`
+export const Number = styled.div`
+background: #e1dcd5;
+padding-left: 10px;
+border-top-left-radius: 9px;
+border-top-right-radius: 9px;
+`
+
+export const dfsd = styled.div`
+display: flex;
+flex-wrap: wrap;
+gap: 10px;
+`
+export const dfskf2 = styled.div``
+export const OFDjsfdos = styled.div`
+width: 200px;
+
+`
