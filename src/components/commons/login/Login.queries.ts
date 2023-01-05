ee4cd6147923668999bccd6a457bf4557a2b8f4b
JJHaEun <yj03057@gmail.com>
2023-01-05 12:39:09 +0900

    2023-01-05-(#147. gql설정추가)

--- src/components/commons/login/Login.queries.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const LOG_IN = gql`
-    mutation login($email:String! $password:String!){
-        login(email:$email password:$password){
-
-        }
-    }
-`;
